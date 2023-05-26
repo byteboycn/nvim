6a5a19cd70f622ba8e5b51b9e7b628d6154ce47f
hongshaochuan <hongshaochuan@icc.link>
2023-05-26 14:53:12 +0800

    config

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 require("kide.core")
 require("kide.plugins")
 
--- 自动切换输入法
+-- 自动切换输入法 (更多事件参考：https://neovim.io/doc/user/autocmd.html#autocmd-events)
 vim.cmd('autocmd InsertLeave * :silent :!inputsource com.apple.keylayout.ABC')
 vim.cmd('autocmd VimEnter * :silent :!inputsource com.apple.keylayout.ABC')
-
+vim.cmd('autocmd FocusGained * :silent :!inputsource com.apple.keylayout.ABC')
